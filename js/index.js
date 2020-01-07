1292448c9f29d9747865dadc57858ffe9d29aeb5
srinivasan V <srini@techmindsllc.com>
2020-01-07 16:32:53 +0530

    Added javascript

---
+++ js/index.js
@@ -0,0 +1,19 @@
+/**
+ * Description: It divides the vaulue based on the given two inputs
+ */
+function divide(paramA, paramB) {
+    var x = paramA;
+    var y = paramB;
+    var z = x / y;
+    return z;
+}
+
+/**
+ * Description: It divides the vaulue based on the given two inputs
+ */
+function divide(paramA, paramB) {
+    var x = paramA;
+    var y = paramB;
+    var z = x / y;
+    return z;
+}
